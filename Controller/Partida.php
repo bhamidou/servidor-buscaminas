93941e17eedbe2b6db41d990f6f1e5a5e9b350d5
badr <badrhamidou@gmail.com>
2023-10-02 22:11:48 +0200

    update Partida and index

--- Controller/Partida.php
+++ Controller/Partida.php
@@ -1,17 +1,64 @@
 <?php
 
+require_once 'Constantes.php';
+require_once 'Conexion.php';
+
 class Partida{
 
     public function getPartida(){
+        $con = new Conexion();
+        $con->conectar();
 
-    }  
+        $consulta = "SELECT * FROM PARTIDA";
+
+        $stmt = mysqli_prepare(Conexion::$conexion, $consulta);
+
+        mysqli_stmt_execute($stmt);
+        $resultados = mysqli_stmt_get_result($stmt);
+        $con->desconectar();
+
+        $rtn= [];
+
+        while ($fila = mysqli_fetch_row($resultados)) {
+               $rtn[] = $fila[0].",". $fila[1].",".$fila[2].",".$fila[3];
+        }
+
+        print_r($rtn);
+    }
 
     public function updatePartida($posicion){
-        
-    } 
+        $con = new Conexion();
+        $con->conectar();
+
+        $con->desconectar();
+    }
     
     public function insertPosPartida(){
-        
-    } 
+        $con = new Conexion();
+        $con->conectar();
+
+        $con->desconectar();
+    }
+
+    public function getTableroInvisible($idUser){
+        $con = new Conexion();
+        $con->conectar();
+
+        $consulta = "SELECT * FROM PARTIDA WHERE idUsuario = ? ";
+
+        $stmt = mysqli_prepare(Conexion::$conexion, $consulta);
+        mysqli_stmt_bind_param($stmt, "s", $idUser);
+        mysqli_stmt_execute($stmt);
+        $resultados = mysqli_stmt_get_result($stmt);
+        $con->desconectar();
+
+        $rtn= [];
+
+        while ($fila = mysqli_fetch_row($resultados)) {
+               $rtn[] = $fila[3];
+        }
+
+        print_r($rtn);
+    }
 
 }
--- index.php
+++ index.php
@@ -1,3 +1,83 @@
 <?php
 
 
+require_once './Controller/Partida.php';
+
+header("Content-Type:application/json");
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+$paths = $_SERVER['REQUEST_URI'];
+
+$v = explode('/', $paths);
+
+unset($v[0]);
+
+$cod = 200;
+$mesg = "todo bien";
+
+switch ($requestMethod) {
+    case 'GET': {
+            if (!empty($v[1])) {
+                $p = new Partida();
+                $idUser = $v[1];
+                $p->getTableroInvisible($idUser);
+            } else {
+                $cod = 406;
+                $mesg = "ERROR ID USER";
+                echo json_encode(['cod' => $cod, 'mesg' => $mesg]);
+            }
+        }
+        break;
+
+    case 'POST': {
+        // if (!empty($v[1])) {
+        //     $p = new Partida();
+        //     $idUser = $v[1];
+        //     $p->getTableroInvisible($idUser);
+
+        // } else {
+        //     $cod = 406;
+        //     $mesg = "ERROR ID USER";
+        //     echo json_encode(['cod' => $cod, 'mesg' => $mesg]);
+        // }
+    }
+
+    break;
+
+    case 'DELETE' :{
+        // if (!empty($v[1])) {
+        //     $p = new Partida();
+        //     $idUser = $v[1];
+        //     $p->getTableroInvisible($idUser);
+        // } else {
+        //     $cod = 406;
+        //     $mesg = "ERROR ID USER";
+        //     echo json_encode(['cod' => $cod, 'mesg' => $mesg]);
+        // }
+    }
+
+    break;
+
+    case 'UPDATE': {
+        // if (!empty($v[1])) {
+        //     $p = new Partida();
+        //     $idUser = $v[1];
+        //     $p->getTableroInvisible($idUser);
+        // } else {
+        //     $cod = 406;
+        //     $mesg = "ERROR ID USER";
+        //     echo json_encode(['cod' => $cod, 'mesg' => $mesg]);
+        // }
+    }
+    
+    break;
+
+    default: {
+            $cod = 405;
+            $mesg = "METHOD NOT SUPPORTED YET";
+            echo json_encode(['cod' => $cod, 'mesg' => $mesg]);
+        }
+}
+
+
+header("HTTP/1.1 $cod $mesg");
