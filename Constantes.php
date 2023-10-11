b56b338a21585894ae18c86a6e0bd774267336d5
badr <badrhamidou@gmail.com>
2023-10-11 10:00:25 +0200

    revert Constantes.php

---
+++ Constantes.php
@@ -0,0 +1,9 @@
+<?php
+
+class Constantes {
+    static $host = "localhost";
+    static $user = "root";
+    static $pass = "1234";
+    static $database = "buscaminas";
+    static $port = "3333";
+}
