27b62baaf7e16aa9f631cc7d04564795b4d4d5b4
badr <badrhamidou@gmail.com>
2023-10-18 01:33:17 +0200

    fix vars of conectar()

--- Controller/Conexion/Conexion.php
+++ Controller/Conexion/Conexion.php
@@ -6,12 +6,12 @@ require __DIR__.'../../../Constantes.php';
 class Conexion
 {
 
-    static $conexion;
+    public static $conexion;
 
     public function conectar()
     {
         try {
-            self::$conexion = new mysqli(Constantes::$host, Constantes::$user, Constantes::$pass, Constantes::$database, Constantes::$port);
+            self::$conexion = new mysqli(Constantes::$DB_host, Constantes::$DB_user, Constantes::$DB_pass, Constantes::$DB_database, Constantes::$DB_port);
         } catch (Exception $e) {
             die();
         }
